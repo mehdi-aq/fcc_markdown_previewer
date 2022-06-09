b92ee6d61220137f43ecd1826afc683c63f57446
mehdi_aq <douahmehdi.p@gmail.com>
2022-06-09 03:12:15 +0200

    Initial commit

---
+++ precache-manifest.d97a3af04be6de56a3d033f362d9cde3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718b97eb5e187c8a8833bb44c99b5fbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "777f702dfb4fe3e6867c",
+    "url": "/static/css/main.4af1de04.chunk.css"
+  },
+  {
+    "revision": "01c3413f74cd3f51bb48",
+    "url": "/static/js/2.61f0d6a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.61f0d6a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "777f702dfb4fe3e6867c",
+    "url": "/static/js/main.fa41b544.chunk.js"
+  },
+  {
+    "revision": "c7076c5f51ae37e38b85",
+    "url": "/static/js/runtime-main.ff899319.js"
+  }
+]);
